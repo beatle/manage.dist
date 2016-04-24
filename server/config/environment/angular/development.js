a12cbdff1c13e3872ae41e135e04b3bd1263731a
Manjesh Vinayaka <manjesh@quetzal.in>
2016-04-24 20:17:42 +0530

    more

--- server/config/environment/angular/development.js
+++ server/config/environment/angular/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports = module.exports = {
-  QUARC_API: 'http://api.quezx.dev/api',
+  QUARC_API: 'http://localhost:9000/api',
   PARTNER_OAUTH_API: 'http://api.quezx.dev/applications/partner/api',
   ACCOUNTS: '//accounts.quezx.dev',
   OAUTH: '//accounts.quezx.dev/authorise?client_id=partnerquezx&response_type=code&' + 'redirect_uri=http://partner.quezx.dev/access/oauth&state=yo',
