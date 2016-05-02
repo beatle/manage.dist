40a762e45e68db9fd6d79d2b004bb33c99b2eaa2
Manjesh Vinayaka <manjesh@quetzal.in>
2016-05-03 02:01:46 +0530

    oauth

--- server/config/environment/angular/development.js
+++ server/config/environment/angular/development.js
@@ -2,6 +2,7 @@
 
 exports = module.exports = {
   QUARC_API: 'http://localhost:9000/api',
+  OAUTH_URL: 'http://localhost:9000',
   PARTNER_OAUTH_API: 'http://api.quezx.dev/applications/partner/api',
   ACCOUNTS: '//accounts.quezx.dev',
   OAUTH: '//accounts.quezx.dev/authorise?client_id=partnerquezx&response_type=code&' + 'redirect_uri=http://partner.quezx.dev/access/oauth&state=yo',
--- server/config/environment/angular/production.js
+++ server/config/environment/angular/production.js
@@ -2,6 +2,7 @@
 
 exports = module.exports = {
   QUARC_API: 'http://ayyayo.com:3010/api',
+  OAUTH_URL: 'http://ayyayo.com:3010',
   PARTNER_OAUTH_API: 'https://qapi.quezx.com/applications/partner/api',
   ACCOUNTS: '//accounts.quezx.com',
   OAUTH: '//accounts.quezx.com/authorise?client_id=partnerquezx&response_type=code&' + 'redirect_uri=https://partner.quezx.com/access/oauth&state=yo',
