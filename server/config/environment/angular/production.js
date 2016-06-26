e1f725821409290f60b1a48ce6291004e5cec2c7
Manjesh Vinayaka <manjesh@quetzal.in>
2016-06-26 22:57:13 +0530

    https

--- server/config/environment/angular/production.js
+++ server/config/environment/angular/production.js
@@ -1,8 +1,8 @@
 'use strict';
 
 exports = module.exports = {
-  QUARC_API: 'http://api.beatleanalytics.com/api',
-  OAUTH_URL: 'http://api.beatleanalytics.com',
+  QUARC_API: 'https://api.beatleanalytics.com/api',
+  OAUTH_URL: 'https://api.beatleanalytics.com',
   PARTNER_OAUTH_API: 'https://qapi.quezx.com/applications/partner/api',
   ACCOUNTS: '//accounts.quezx.com',
   OAUTH: '//accounts.quezx.com/authorise?client_id=partnerquezx&response_type=code&' + 'redirect_uri=https://partner.quezx.com/access/oauth&state=yo',
