a96dab170f592f527d07c03da5fd95e6f717c02b
Manjesh Vinayaka <manjesh@quetzal.in>
2016-07-18 00:55:10 +0530

    starting es

--- server/config/environment/angular/development.js
+++ server/config/environment/angular/development.js
@@ -2,6 +2,7 @@
 
 exports = module.exports = {
   QUARC_API: 'http://localhost:9000/api',
+  BEATLE: 'http://localhost:3003/#',
   OAUTH_URL: 'http://localhost:9000',
   PARTNER_OAUTH_API: 'http://api.quezx.dev/applications/partner/api',
   ACCOUNTS: '//accounts.quezx.dev',
