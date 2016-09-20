0c91fc299694a689db3e9decd402d67d1671882b
Manjesh V-cp2 <manjeshpv@gmail.com>
2016-09-20 08:31:30 +0530

    b0f58e63fa Update README.md

--- client/bower_components/ui-load/dist/ui-jp.config.js
+++
@@ -1,7 +0,0 @@
-// lazyload config
-
-var jp_config = {
-    thisComponent: ['/theme/bower_components/thisComponent/thisComponent.js',
-        '/theme/bower_components/thisComponent/dist/thisComponent.css']
-
-};
